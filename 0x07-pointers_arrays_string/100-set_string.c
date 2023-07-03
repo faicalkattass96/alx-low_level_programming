29d5734dac640ace1ff6b7c8c96d46dc68d156bf
faicalkattass96 <faicalkattass1@gmail.com>
2023-07-03 15:13:04 -0700

    task 9

--- 0x07-pointers_arrays_string/100-set_string.c
+++ 0x07-pointers_arrays_string/100-set_string.c
@@ -2,10 +2,11 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
- * Return: no return.
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
  */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
